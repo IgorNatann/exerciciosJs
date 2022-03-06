46a9bc0af81e096e6058072756383a4559a6287b
Igor Natan <igornatan4@gmail.com>
2022-03-06 16:30:00 -0300

    conteudo estudado 06-03

---
+++ fundamentos/experimentos.js
@@ -0,0 +1,18 @@
+let a = 3;
+
+global.b = 123; // criando uma variável e atribuindo ela global
+
+this.c = 456;
+this.d = false;
+this.e = "teste";
+
+console.log(a);
+console.log(global.b);
+console.log(this.c);
+console.log(module.exports.c);
+console.log(module.exports === this);
+console.log(module.exports);
+
+// criando uma variável maluca: sem var ou let.
+abc = 3 // Nunca criar variável dessa forma.
+console.log(global.abc);
---
+++ fundamentos/ternario.js
@@ -0,0 +1,7 @@
+// Operador ternario é composto por três partes
+// 1 -  A condição, 2 - a afirmação e 3 - negação
+
+const resultado = nota => nota >= 7 ? "Aprovado" : "Reprovado";
+
+console.log(resultado(7.1));
+console.log(resultado(6.7));
