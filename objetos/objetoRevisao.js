097c8ea64f2a7155847f9b4c75db8af7fa914f1d
Igor Natan <72422447+IgorNatann@users.noreply.github.com>
2022-04-12 20:31:15 -0300

    conteudo estudado 12-04

---
+++ objetos/objetoRevisao.js
@@ -0,0 +1,8 @@
+// Objeto é uma coleção dinâmica de pares chave/valor
+
+const produtor = new Object // Criando novo objeto
+produto.nome= 'Cadeira' // Criando um objeto
+produto['marca do produto'] = 'Generica'
+produto.preco = 220
+
+console.log(produto)
---
+++ objetos/oo.js
@@ -0,0 +1,27 @@
+/*--- INTRODUÇÃO A ORIENTAÇÃO A OBJETOS ---*/
+
+// Procedural
+
+/*
+O que é a OO?
+Dentro da OO tudo se resume em objetos,onde os objetos tem valor e atributos e dentro dele também temos funções que estão exucutando alguma função!
+Um objeto pode se relacionar com o outro, onde ambos podem ser diferentes mas se comunicarem, onde também é possível um objeto erdar herança do outro.
+
+Principios importantes do OO:
+ABSTRAÇÃO, se beseia no poder de fatorar um objeto real, pensando em todos os atributos e funções que esse objeto tem de forma simplificada.
+
+Sendo necessário pensar sempre no cenário que está sendo aplicado, pois podemos aplicar um objeto em um cenário de uma forma, e em um outro de outra forma.
+
+ENCAPSULAMENTO, se resume em fazer o ocultamento de informações que não são necessárias aparecer para o úsuario final, pois não é necessário que seja de conhecimento do mesmo.
+Podemos pensar em cenários de atualizações, onde não é necessário que o úsuario saiba o que está sendo modificado a fundo, e sim apenas informado sobre a mudança para que o mesmo consiga se adaptar ao novo cenário.
+
+Herança (É UM - PROTOTYPE)
+Podemos faer a diferenciação em objetos, pode criando um objeto motor que contem todas as informções dentro do moto, e após isso associar ao carro, não sendo necessário criar as caracteristicas do motor dentro do carro. Onde Carro vai herdar os atributos do motor.
+
+é um ponto importante notar que é mais viável usar sempre a relação é um= composição, por que podemos mudar o cenário sem mexer aos demais pontos ligados no projeto.
+
+Polimorfismo = significa multiplas formas. 
+O intuito de polimorfismo é fazer com que um objeto seja manipulado de forma fácil, 
+onde eu posso criar um obbjeto do tipo carro e atribuir atributos sobre carro,  usar o mesmo objeto para criar carros de outras marcas, mas utilizando a mesma estrutura. Isso facilita e faz com que um único código seja utilizado em diversos cenários.
+
+*/
